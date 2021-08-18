54bb3c22f3fc1e4ff41920aac9eb29b632a8c619
Osvasan19 <A01641227@itesm.mx>
2021-08-18 22:05:52 +0000

    Actividad 1

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,8 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
+    nacimiento=int(input("Dame el año de nacimiento: "))
+    actual=int(input("Dame el año actual: "))
+    lustros=(actual-nacimiento)/5
+    print("Los lustros que has vivido son:",lustros) 
 
 if __name__ == '__main__':
     main()
--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
+    mensajes=int(input("Dame el número de mensajes: "))
+    megas=float(input("Dame el número de megas: "))
+    minutos=int(input("Dame el número de minutos: "))
+    mensual=(mensajes+megas+minutos)*0.80
+    print("El costo mensual es:",mensual)
 
 if __name__ == '__main__':
     main()
